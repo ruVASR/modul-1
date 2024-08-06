697c9be73459f4ea6c1d445bbb593f7871872a3a
ruVASR <ctivkop@gmail.com>
2024-08-06 08:12:57 +0500

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,8 +1,12 @@
-my_string = input()
+my_string = input('Введите ваше ФИО: ')
 print(len(my_string))
 
 print(my_string.upper())
 
 print(my_string.lower())
 
-print(my_string)
+print(my_string.replace(' ', ''))
+
+print(my_string[0])
+
+print(my_string[-1])
